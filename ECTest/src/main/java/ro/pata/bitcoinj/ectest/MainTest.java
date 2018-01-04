b3d98c8fe6f8a6a18ddc591b6a54460c1e36f2e7
Adi <ohamoha@gmail.com>
2018-01-04 12:00:02 +0200

    Test key generation and sign on external card.

--- ECTest/src/main/java/ro/pata/bitcoinj/ectest/MainTest.java
+++ ECTest/src/main/java/ro/pata/bitcoinj/ectest/MainTest.java
@@ -10,6 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.bitcoinj.core.*;
 import static org.bitcoinj.core.Utils.HEX;
+import org.spongycastle.util.encoders.Hex;
 
 /**
  *
@@ -17,16 +18,30 @@ import static org.bitcoinj.core.Utils.HEX;
  */
 public class MainTest {
     public static void main(String[] args) throws IOException{
-        ECKey key=ECKey.fromASN1(Files.readAllBytes(Paths.get("key.der")));
-        System.out.println(key.toStringWithPrivate(null, null));
+        Smartcard sc=new Smartcard();
+        //sc.test();        
+        testSig();
+    }
+    
+    public static void testSig() throws IOException{
+        //ECKey key=ECKey.fromASN1(Files.readAllBytes(Paths.get("key.der")));
+        //ECKey key=ECKey.fromPrivate(Hex.decode("EB9361012C48490B3E8FC0E4C9BE3D763ABBC42D3C23CDE729E02D29720AFF78"));
+        ECKey key=ECKey.fromPublicOnly(Hex.decode("04013F3343BD902785500B4B548B1555C263EF75D75869625AA7C782645BD79DE07CFD8B3C4D10B0EAB8BB4A3259FECF0935004E51720013393F24637BFA062678"));
+        System.out.println(key.decompress().toStringWithPrivate(null, null));
+        
+        
+        
+        
         
         byte[] msg="ADI".getBytes();
         System.out.println("msg: "+HEX.encode(msg));
         Sha256Hash msgh=Sha256Hash.of(msg);
         System.out.println("msgh: "+msgh);
         
-        ECKey.ECDSASignature sig=key.sign(msgh);
+        ECKey.ECDSASignature sig;//=key.sign(msgh);       
+        sig=ECKey.ECDSASignature.decodeFromDER(Hex.decode("30450221008E5CA6F321F19A4B987FBFBDBAAA5C418F5F999850617350EFAEAD3D75560EBD02203722C5FCEBDFD2B993AF3FD0EFD95D1F285604D727096DEDE8F2010475557BAA"));
         
+      
         System.out.println("verif: "+key.verify(msgh, sig)); 
 
         
